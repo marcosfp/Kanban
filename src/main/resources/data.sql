54384f109da7a115135b5eeb6bb507a1ade183c4
madrid <madrid@DESKTOP-FD6ANVC>
2022-02-22 21:28:10 +0100

    añadiendo archivos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('luis', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('erik', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('sergio', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('alejandro', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('moha', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('david', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('jessica', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('salma', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('christopher', '12345678');
+INSERT INTO usuario (nombre_usuario, contrasena) VALUES ('marcos', '12345678');
+
+INSERT INTO rol (id_rol, nombre_rol) VALUES ('1', 'manager');
+INSERT INTO rol (id_rol, nombre_rol) VALUES ('2', 'trabajador');
+
+insert into usuario_rol (id_usuario, id_rol ) values (1,1);
+insert into usuario_rol (id_usuario, id_rol ) values (2,1);
+insert into usuario_rol (id_usuario, id_rol ) values (3,1);
+insert into usuario_rol (id_usuario, id_rol ) values (4,1);
+insert into usuario_rol (id_usuario, id_rol ) values (5,1);
+insert into usuario_rol (id_usuario, id_rol ) values (6,1);
+insert into usuario_rol (id_usuario, id_rol ) values (7,1);
+insert into usuario_rol (id_usuario, id_rol ) values (8,1);
+insert into usuario_rol (id_usuario, id_rol ) values (9,1);
+insert into usuario_rol (id_usuario, id_rol ) values (10,2);
