26a33da60c9098b6e9224a8830ac802b1ad6c5fb
marco <marcosfppf@gmail.com>
2022-02-23 16:21:06 +0100

    Cambios hehcos en casa.

--- src/main/java/des/kanban/controladores/IndexController.java
+++ src/main/java/des/kanban/controladores/IndexController.java
@@ -1,5 +1,28 @@
 package des.kanban.controladores;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class IndexController {
 
+	
+	@GetMapping("/index")
+	public ModelAndView getIndex () {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		
+		return mav;
+	}
+	
+	@GetMapping("/login")
+	public ModelAndView getLogin () {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login");
+		
+		return mav;
+	}
+	
+	
 }
--- src/main/java/des/kanban/controladores/UsuarioController.java
+++ src/main/java/des/kanban/controladores/UsuarioController.java
@@ -1,5 +1,22 @@
 package des.kanban.controladores;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("usuario")
 public class UsuarioController {
 
+	
+	@GetMapping("/alta")
+	public ModelAndView getAlta () {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("usuario/alta");
+		
+		return mav;
+	}
+	
+	
 }
