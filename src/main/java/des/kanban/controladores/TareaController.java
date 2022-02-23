71675d4d1ab5a8a4608cd7d8bd4da05555dd6717
marco <marcosfppf@gmail.com>
2022-02-23 16:39:08 +0100

    añadiendo perfil prouyecto

--- src/main/java/des/kanban/controladores/TareaController.java
+++ src/main/java/des/kanban/controladores/TareaController.java
@@ -1,5 +1,21 @@
 package des.kanban.controladores;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("tarea")
 public class TareaController {
 
+	
+	@GetMapping("/index")
+	public ModelAndView getIndex () {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		
+		return mav;
+	}
+	
 }
